580da27ba87db5900bcc7db5819faef9e30afbb1
WeiChien Hsu <weichien711@gmail.com>
2017-10-09 16:12:37 -0500

    add functions

--- cash_Register.js
+++ cash_Register.js
@@ -1,11 +1,3 @@
-
-function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
-}
-
-// output [["TWENTY",20.00],["TEN",10.00],etc.]
-function changeCash(total) {
   var changes = {
     TWENTY:20.00,
     TEN:10.00,
@@ -16,6 +8,33 @@ function changeCash(total) {
     NICKEL:0.05,
     PENNY:0.01
   };
+
+
+function checkCashRegister(price, cash, cid) {
+  var change = fpFix(cash - price);
+  var totalChange = readTotal(cid);
+  if(change == totalChange){
+    return "Closed";
+  } else if(change > totalChange){
+    return "Insufficient Funds";
+  } else{
+    return changeCash(change);
+  }
+  
+  
+  return ;
+}
+
+function readTotal(arr){
+  var total = 0;
+  
+  for(var j = 0; j < arr.length; j++ ){
+     total = fpFix(total + arr[j][1]); 
+  }
+  return total;
+}
+
+function changeCash(total) {
   
   var change = ["TWENTY","TEN","FIVE","ONE","QUARTER","DIME","NICKEL","PENNY"];
   var result = [];
@@ -30,7 +49,7 @@ function changeCash(total) {
         num = Math.floor(total/c);
         total = fpFix(total - num*c);
         output = parseFloat(Math.round(num*c * 100) / 100).toFixed(2);
-        result.push([change[i],output]); 
+        result.push([change[i],Number(output)]); 
       }     
     } //for
   } //while
@@ -41,17 +60,7 @@ var fpFix = function (n) {
     return Math.round(n * 100)/100;
 };
 
-changeCash(42.11);
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.10],
-// ["QUARTER", 4.25],
-// ["ONE", 90.00],
-// ["FIVE", 55.00],
-// ["TEN", 20.00],
-// ["TWENTY", 60.00],
-// ["ONE HUNDRED", 100.00]]
 
-// checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
